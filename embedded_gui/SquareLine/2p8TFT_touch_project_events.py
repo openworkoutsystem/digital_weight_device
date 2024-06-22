8c13135e08e57d17215dc34013bfd8901eace053
Aaron <aaron@lunarmass.com>
2024-06-22 16:24:39 -0700

    feat: Add new files for SquareLine GUI and fonts
    The commit adds new files for the SquareLine GUI, including event handling functions and font configuration files.

---
+++ embedded_gui/SquareLine/2p8TFT_touch_project_events.py
@@ -0,0 +1,16 @@
+
+def setinvgpio(event_struct):
+    return
+
+
+def cmdActivate(event_struct):
+    return
+
+
+def cmdRepo(event_struct):
+    return
+
+
+def cmdRegen(event_struct):
+    return
+
---
+++ embedded_gui/SquareLine/amoled_1p9_events.py
@@ -0,0 +1,4 @@
+
+def weightUnitChange(event_struct):
+    return
+
