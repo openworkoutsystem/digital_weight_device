edab8923602931c2b3e90afa0d3b1bcd63b6ad9a
Aaron <aaron.willey@gmail.com>
2025-10-03 21:54:17 -0700

    adding initial rtd

---
+++ docs/source/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Open Workout System API'
+copyright = '2025, Aaron'
+author = 'Aaron'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For C++-style comments if you add Doxygen later
+    'sphinxcontrib.httpdomain',  # For pretty HTTP examples
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'  # Or 'furo' for modern look—install via requirements.txt
+html_title = 'Open Workout System API'
+html_static_path = ['_static']
---
+++ docs/source/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to Open Workout System API Documentation!
+===============================================
+
+This documents the firmware APIs for the ESP32-based digital weight device.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
